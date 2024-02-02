d277aeed540d53e3942292ab1fd858f960eb6bed
salgadoIbacache <se.salgado@duocuc.cl>
2024-02-02 11:04:02 -0300

    Se agrega el Logo a la app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Logo from './img/logo.png'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='app-note'>
+      <div className='logo-container'>
+        <img 
+          src={ Logo } 
+          className='logo' 
+          alt='Logo' 
+        />
+      </div>
     </div>
   );
 }
